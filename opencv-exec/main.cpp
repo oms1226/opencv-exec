6137d4e93c1b948bc47491daa13d2e3b180eb28d
오명섭 <oms1226@gmail.com>
2019-01-18 07:31:09 +0900

    Initial Commit

---
+++ opencv-exec/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv-exec
+//
+//  Created by oms1226 on 18/01/2019.
+//  Copyright © 2019 oms1226. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
